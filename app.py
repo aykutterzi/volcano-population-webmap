d39237d32b78efd37c445fa5600aec9e339dee02
aykutterzi <terziaykut@hotmail.com>
2021-04-28 15:06:34 +0300

    Add Pointer and color for different altitudes

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 import folium
+import pandas
 
-map = folium.Map(location=[37, 15], zoom_start=6, tiles="Stamen Terrain")
+data = pandas.read_csv("./Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
 
+
+def color_generator(elevation):
+    if elevation < 100:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[37, 15], [41, 14]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi, Etna here!", icon=folium.Icon(color="green")))
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    data_iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fill_color = color_generator(el), color="grey", fill_opacity=0.7)
+    fg.add_child(
+        folium.Marker(location=[lt, ln],radius=6, popup=folium.Popup(data_iframe), icon=folium.Icon(color=color_generator(el)))
+    )
 
 
-map.add_child((fg))
+
+map.add_child(fg)
 
 map.save("Map1.html")
