f0b44e318143bba300e1ec4adbcfd59f695f9655
aykutterzi <terziaykut@hotmail.com>
2021-04-28 16:02:16 +0300

    Add layer control panel

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ Height: %s m
 """
 
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     data_frame = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(
+    fgv.add_child(
         folium.CircleMarker(
             location=[lt, ln],
             radius=6,
@@ -39,8 +39,9 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
             fill_opacity=0.7,
         )
     )
+fgp = folium.FeatureGroup(name="Population")
 
-fg.add_child(
+fgp.add_child(
     folium.GeoJson(
         data=open("world.json", "r", encoding="utf-8-sig").read(),
         style_function=lambda x: {
@@ -52,7 +53,10 @@ fg.add_child(
         },
     )
 )
+map.add_child(fgv)
+map.add_child(fgp)
 
-map.add_child(fg)
+map.add_child(folium.LayerControl())
+
 
 map.save("Map1.html")
