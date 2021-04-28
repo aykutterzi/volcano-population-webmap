75b821a64b64728f2b15c9a2fc620b876367e031
aykutterzi <terziaykut@hotmail.com>
2021-04-28 15:35:01 +0300

    Add polygon layers and world json data

--- app.py
+++ app.py
@@ -27,12 +27,20 @@ map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
-    data_iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fill_color = color_generator(el), color="grey", fill_opacity=0.7)
+    data_frame = folium.IFrame(html=html % (name, name, el), width=200, height=100)
     fg.add_child(
-        folium.Marker(location=[lt, ln],radius=6, popup=folium.Popup(data_iframe), icon=folium.Icon(color=color_generator(el)))
+        folium.CircleMarker(
+            location=[lt, ln],
+            radius=6,
+            popup=folium.Popup(data_frame),
+            icon=folium.Icon(color=color_generator(el)),
+            fill_color=color_generator(el),
+            color="grey",
+            fill_opacity=0.7,
+        )
     )
 
+fg.add_child(folium.GeoJson(data=(open("world.json", "r", encoding="utf-8-sig").read())))
 
 
 map.add_child(fg)
